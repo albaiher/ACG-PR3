ed6eb708dfce9a99ecd9713068094460f1509c05
ALBAIHER <albaiher@alumno.upv.es@ldsic-vdi16.upvnet.upv.es>
2023-05-08 17:47:01 +0200

    LastZeroTest

--- test/LastZeroTest.java
+++ test/LastZeroTest.java
@@ -15,4 +15,18 @@ public class LastZeroTest
       int arr[] = {0, 1, 0};
       assertEquals("Multiple zeroes: should find last one", 2, LastZero.lastZero(arr));
    }
+
+   // this test fails!
+   @Test public void noZeroes() 
+   {
+      int arr[] = {9, 1, 2};
+      assertEquals("Multiple zeroes: should find last one", -1, LastZero.lastZero(arr));
+   }
+
+   // this test fails!
+   @Test public void oneZero() 
+   {
+      int arr[] = {0, 1, 2};
+      assertEquals("Multiple zeroes: should find last one", 0, LastZero.lastZero(arr));
+   }
 }
